d03e990d847a91052e41c5f694e3f6baad482b72
niels <nielsr2@gmail.com>
2019-05-03 12:31:54 +0200

    todo for dear old mikz

--- src/main/java/org/fxmisc/richtext/demo/richtext/OverlayPane.java
+++ src/main/java/org/fxmisc/richtext/demo/richtext/OverlayPane.java
@@ -8,7 +8,7 @@ public class OverlayPane extends BorderPane {
 
     OverlayPane() {
         this.setPrefSize(Settings.WINDOW_WIDTH, Settings.WINDOW_HEIGHT);
-//        this.setMouseTransparent(true);
+//        this.setMouseTransparent(true); // TODO MIKKEL, SO WITH THIS DISABLED, WE CANNOT CLICK TOOLBAR ICONS, WITH IT ENABLED WE CANNOT CLICK KBS'. U go make clever code.
         km = new KBSManager();
         this.setId("overlayPane");
         this.setRight(km);
