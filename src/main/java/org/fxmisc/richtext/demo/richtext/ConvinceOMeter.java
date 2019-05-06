8ad4fcbdea70e94f07b7e7b0a781c29cf34184a9
Mikkel Kappel Persson <43847844+MikkelKPersson@users.noreply.github.com>
2019-05-06 16:24:51 +0200

    ConvinceOMeter

---
+++ src/main/java/org/fxmisc/richtext/demo/richtext/ConvinceOMeter.java
@@ -0,0 +1,58 @@
+package org.fxmisc.richtext.demo.richtext;
+
+import javafx.geometry.Insets;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+
+public class ConvinceOMeter extends Pane {
+
+    Rectangle backgroundRect;
+
+
+    private float timesSlower;
+    String argument;
+    private Text text = new Text(argument);
+
+
+    ConvinceOMeter(float timesSlower){
+
+        this.setVisible(true);
+
+        this.timesSlower = timesSlower;
+        backgroundRect = new Rectangle(170, 50, Color.LIGHTGREY);
+
+       /* HBox content = new HBox(5);
+        content.setPadding(new Insets(5, 5, 5, 5));
+
+
+
+        content.getChildren().addAll(icon, this.shortcut);*/
+
+
+//       this.text("You are " + this.timesSlower + " times slower by not using this shortcut" );
+        this.showText1();
+        TextFlow textFlow = new TextFlow(text);
+
+        this.getChildren().addAll(backgroundRect, textFlow);
+
+
+
+    }
+
+    public Text showText1(){
+        this.setVisible(true);
+        text.setText( "You are " + this.timesSlower + " times slower by not using this shortcut");
+        return text;
+
+    }
+
+
+
+}
