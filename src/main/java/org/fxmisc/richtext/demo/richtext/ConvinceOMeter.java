f4008a3526a177d47cba81d992b2aba03a39783e
Mikkel Kappel Persson <43847844+MikkelKPersson@users.noreply.github.com>
2019-05-09 14:28:14 +0200

    Stuff ConvinceOMeter

--- src/main/java/org/fxmisc/richtext/demo/richtext/ConvinceOMeter.java
+++ src/main/java/org/fxmisc/richtext/demo/richtext/ConvinceOMeter.java
@@ -1,6 +1,8 @@
 package org.fxmisc.richtext.demo.richtext;
 
-import javafx.scene.layout.Pane;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
@@ -9,9 +11,15 @@ import javafx.scene.text.TextFlow;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-public class ConvinceOMeter extends Pane {
+import static javafx.scene.paint.Color.DARKGRAY;
+import static javafx.scene.text.TextAlignment.CENTER;
+
+public class ConvinceOMeter extends StackPane {
 
     Rectangle backgroundRect;
+
+    int width = 170;
+    int height = 50;
 
     private double timesSlower;
     String argument;
@@ -20,18 +28,31 @@ public class ConvinceOMeter extends Pane {
 
 
     ConvinceOMeter(float timesSlower){
-      this.setVisible(false);
+
+
+        this.setVisible(false);
       this.setManaged(false);
+        this.setAlignment(Pos.CENTER);
+
 
 
 
 
         this.timesSlower = timesSlower;
-        backgroundRect = new Rectangle(170, 50, Color.LIGHTGREY);
+        backgroundRect = new Rectangle(width, height, Color.LIGHTGREY);
+        backgroundRect.setArcHeight(100);
+        backgroundRect.setArcWidth(40);
+        backgroundRect.setStroke(DARKGRAY);
+
 
         this.showText1();
+
+
         TextFlow textFlow = new TextFlow(text);
 
+        textFlow.setPrefWidth(width);
+        textFlow.setTextAlignment(CENTER);
+        textFlow.setPadding(new Insets(7, 10, 7, 10));
 
         this.getChildren().addAll(backgroundRect, textFlow);
 
@@ -40,11 +61,11 @@ public class ConvinceOMeter extends Pane {
     }
 
     public Text showText1(){
+        text.setWrappingWidth(width);
+        String string = Double.toString(this.timesSlower);
+        Text timesSlower = new Text(string);
 
-
-        text.setWrappingWidth(50);
-
-        text.setText( "You are " + this.timesSlower + " times slower by not using this shortcut");
+        text.setText("You are " + string + " times slower by \n not using this shortcut");
         return text;
 
     }
@@ -59,7 +80,6 @@ public class ConvinceOMeter extends Pane {
         BigDecimal bd = new BigDecimal(timesSlower).setScale(1, RoundingMode.HALF_UP);
         double newInput = bd.doubleValue();
         this.timesSlower = newInput;
-
     }
 
 }
