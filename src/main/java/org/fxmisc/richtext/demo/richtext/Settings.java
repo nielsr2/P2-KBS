3c7ba0bed06e6ed0a67c4a800529591b39e6eeca
Kristinn Bragi <kristinnbragi@gmail.com>
2019-05-16 11:57:06 +0200

    Small fix

--- src/main/java/org/fxmisc/richtext/demo/richtext/Settings.java
+++ src/main/java/org/fxmisc/richtext/demo/richtext/Settings.java
@@ -8,9 +8,9 @@ public interface Settings {
     //public final static String WINDOW_TITLE = "SimpleRichTextFX Demo";
 
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    double WINDOW_WIDTH = screenSize.getWidth();
+    double WINDOW_WIDTH = screenSize.getWidth() / 3 * 2;
 
-    double WINDOW_HEIGHT = screenSize.getHeight();
+    double WINDOW_HEIGHT = screenSize.getHeight() / 3 * 2 - 25;
     double PAPER_WIDTH = 600;
     double PAPER_HEIGHT = PAPER_WIDTH * 1.4;
 
