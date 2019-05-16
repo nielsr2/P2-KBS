8484abe84d3e81e589fbb13442ac940983d28963
Kristinn Bragi <kristinnbragi@gmail.com>
2019-05-16 11:43:59 +0200

    Adjusted window size for mac

--- src/main/java/org/fxmisc/richtext/demo/richtext/Settings.java
+++ src/main/java/org/fxmisc/richtext/demo/richtext/Settings.java
@@ -8,9 +8,9 @@ public interface Settings {
     //public final static String WINDOW_TITLE = "SimpleRichTextFX Demo";
 
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    double WINDOW_WIDTH = screenSize.getWidth() / 3 * 2;
+    double WINDOW_WIDTH = screenSize.getWidth();
 
-    double WINDOW_HEIGHT = screenSize.getHeight() / 3 * 2 - 25;
+    double WINDOW_HEIGHT = screenSize.getHeight();
     double PAPER_WIDTH = 600;
     double PAPER_HEIGHT = PAPER_WIDTH * 1.4;
 
